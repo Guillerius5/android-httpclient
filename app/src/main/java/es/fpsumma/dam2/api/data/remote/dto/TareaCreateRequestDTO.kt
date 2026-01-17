5c570746afb4ed3528bf522d009d82a5472fb912
Guillerius5 <guillerhamel@gmail.com>
2026-01-17 19:56:52 +0100

    fin del proyecto

---
+++ app/src/main/java/es/fpsumma/dam2/api/data/remote/dto/TareaCreateRequestDTO.kt
@@ -0,0 +1,6 @@
+package es.fpsumma.dam2.api.data.remote.dto
+
+data class TareCreateRequestDTO(
+    val titulo: String,
+    val descripcion: String
+)
---
+++ app/src/main/java/es/fpsumma/dam2/api/data/remote/dto/TareaUpdateRequestDTO.kt
@@ -0,0 +1,3 @@
+package es.fpsumma.dam2.api.data.remote.dto
+
+data class TareaUpdateRequestDTO()
